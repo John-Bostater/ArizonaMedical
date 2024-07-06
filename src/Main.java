675eb545f012ee73c19a140e78dd28be36b8fc66
RyanClark7 <raclar17@asu.edu>
2024-07-06 16:50:03 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -99,7 +99,7 @@ public class Main extends Application{
   public void start(Stage primaryStage) {
     //Program Loop: 
     //-------------------------------------------------------------------------------------------------------
-      //Title of the program
+      //Title of the program on maim screen
         primaryStage.setTitle("Arizona Medical");
     
       //Load the Welcome Page of the program and continue from there...
